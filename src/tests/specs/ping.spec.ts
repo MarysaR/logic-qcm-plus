7ac3a3296ffffaccd6a2ebfdc149f3980773d897
neycha <regentmarysapro@gmail.com>
2025-07-02 08:16:32 +0200

    fix add test for jest script corrected with lint:fix

--- src/tests/specs/ping.spec.ts
+++ src/tests/specs/ping.spec.ts
@@ -1,5 +1,5 @@
 describe('ping', () => {
-    it('should pass a dummy test', () => {
-        expect(1 + 1).toBe(2);
-    });
+  it('should pass a dummy test', () => {
+    expect(1 + 1).toBe(2);
+  });
 });
