74317171c2600aef1c0222cbd54ba4a96e99025a
neycha <regentmarysapro@gmail.com>
2025-09-23 12:20:50 +0200

    feat: enriched user entity

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -1,8 +1,11 @@
+import { Role } from '../roles/role';
+
 export interface User {
   id: number;
   login: string;
-  password: string;
   email: string;
+  password: string;
   isActive: boolean;
   roleId: number;
+  role: Role;
 }
