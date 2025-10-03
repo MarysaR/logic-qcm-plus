a2610daa0a99ddaff802c98562b70279346103fd
neycha <regentmarysapro@gmail.com>
2025-10-03 14:04:53 +0200

    feat: remove get methods

--- src/interfaces/questionRepository.ts
+++ src/interfaces/questionRepository.ts
@@ -1,11 +1,8 @@
 import { Result } from '../errors/result';
 import { AppError } from '../errors/appError';
-import { Question } from '../entities/question/question';
 import { CreateQuestionCommand } from '../commands/question/createQuestionCommand';
 
 export interface QuestionRepository {
-  getQuestions(questionnaireId: number): Promise<Result<Question[], AppError>>;
-  getQuestionById(id: number): Promise<Result<Question, AppError>>;
   createQuestion(
     command: CreateQuestionCommand
   ): Promise<Result<void, AppError>>;
