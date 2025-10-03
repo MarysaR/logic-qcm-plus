07558805011bc6cb68070fae79de0cc2d7a88c08
neycha <regentmarysapro@gmail.com>
2025-10-03 10:52:48 +0200

    feat: create CreateQuestionUseCase file and export it in the index file

---
+++ src/usecases/question/createQuestionUseCase.ts
@@ -0,0 +1,54 @@
+import { QuestionRepository } from '../../interfaces/questionRepository';
+import { User } from '../../entities/user/user';
+import { AppError } from '../../errors/appError';
+import { RoleEnum } from '../../enums/roleEnums';
+import { Result, Err, Ok } from '../../errors/result';
+import {
+  PermissionDeniedError,
+  TechnicalError,
+  ValidationError,
+} from '../../errors/errors';
+import { CreateQuestionCommand } from '../../commands/question/createQuestionCommand';
+
+export class CreateQuestionUseCase {
+  constructor(private readonly questionRepository: QuestionRepository) {}
+
+  async execute(
+    currentUser: User,
+    command: CreateQuestionCommand
+  ): Promise<Result<void, AppError>> {
+    if (currentUser.role.name != RoleEnum.ADMIN) {
+      return Err.of(
+        new PermissionDeniedError(
+          'Seul un administrateur peut créer une question'
+        )
+      );
+    }
+
+    if (command.answers.length < 2 || command.answers.length > 4) {
+      return Err.of(
+        new ValidationError('Une question doit avoir entre 2 et 4 réponses')
+      );
+    }
+
+    const hasCorrectAnswer = command.answers.some((a) => a.isCorrect);
+    if (!hasCorrectAnswer) {
+      return Err.of(
+        new ValidationError(
+          'Une question doit avoir au moins une réponse correcte'
+        )
+      );
+    }
+
+    const result = await this.questionRepository.createQuestion(command);
+    if (result.isErr()) {
+      return Err.of(
+        new TechnicalError(
+          'Erreur technique lors de la création de la question'
+        )
+      );
+    }
+
+    return Ok.of(undefined);
+  }
+}
