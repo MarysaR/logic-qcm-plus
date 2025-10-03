c68fe9cc0453cd205c203cddc513fcc120717402
neycha <regentmarysapro@gmail.com>
2025-10-03 10:47:06 +0200

    refacto: feat(user): add firstName, lastName, company, createdAt, updatedAt

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -2,10 +2,15 @@ import { Role } from '../roles/role';
 
 export interface User {
   id: number;
+  firstName: string;
+  lastName: string;
   login: string;
   email: string;
   password: string;
+  company?: string;
   isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
   roleId: number;
   role: Role;
 }
